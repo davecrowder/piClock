d0d177ca0a33f81359362ce32a074ee215c6bc50
Dave Crowder <dave@crowder.co.uk>
2020-01-12 18:18:35 +0000

    working

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-<h1>piClock</h1>
-<h2>Summary</h2>
-<p>piClock Provides a program written in Rust to display the current time on a <a href="https://thepihut.com/products/zeroseg">ZeroSeg</a> device attached to a <a href="https://www.raspberrypi.org/products/raspberry-pi-zero-w/">Raspberry Pi Zero W</a>.
+# piClock
+
+## Summary
+
+piClock Provides a program written in Rust to display the current time on a ZeroSeg (<https://thepihut.com/products/zeroseg>) device attached to a <a href="https://www.raspberrypi.org/products/raspberry-pi-zero-w/">Raspberry Pi Zero W</a>.
 <p>The program displays the time in the format "hh-mm-ss" and uses the two buttons on the ZeroSeg board to allow the user to:
 <ul>
 <li>Change the orientation of the display (normal or inverted)</li>
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-use std::time::Duration;
-use std::thread::{sleep, spawn};
+use std::fs;
+use std::io::prelude::*;
 use std::sync::mpsc::{channel, Sender};
+use std::thread::{sleep, spawn};
+use std::time::Duration;
 // use std::convert::TryInto;
 use chrono::prelude::*;
-use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+use rouille::Request;
+
 use rppal::gpio::{Gpio, Level, Trigger};
+use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 // use rppal::spi::*;
 
 fn main() {
     // Open the SPI bus.
     let mut display = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 10_000_000, Mode::Mode0)
         .expect("Failed to create Spi object.");
-    
     // Create a messaging channel for the non-blocking write to SPI bus.
     let (display_tx, display_rx) = channel();
 
@@ -20,8 +26,7 @@ fn main() {
     spawn(move || {
         for (command, value) in display_rx {
             let buffer = [command, value];
-            display.write(&buffer)
-                .expect("Failed to write to SPI.");
+            display.write(&buffer).expect("Failed to write to SPI.");
         }
     });
 
@@ -29,41 +34,47 @@ fn main() {
     let mut display_inverted = false;
     let mut display_intensity = 0xf; //Maximum brightness.
     display_tx.send((0x9, 0x00)).unwrap(); // Disable decode mode for all digits.
-    display_tx.send((0xA, display_intensity)).unwrap();  // Set intensity.
-    display_tx.send((0xB, 0x7)).unwrap();  // Set scan-limit to 0-7.
-    display_tx.send((0xC, 0x1)).unwrap();  // Set normal operation (not shut-domn).
-    display_tx.send((0xF, 0x0)).unwrap();  // Set test mode off.
-
-
+    display_tx.send((0xA, display_intensity)).unwrap(); // Set intensity.
+    display_tx.send((0xB, 0x7)).unwrap(); // Set scan-limit to 0-7.
+    display_tx.send((0xC, 0x1)).unwrap(); // Set normal operation (not shut-domn).
+    display_tx.send((0xF, 0x0)).unwrap(); // Set test mode off.
 
     // Create a messaging channel for main event handler and clone it for each sender.
     let (main_tx, main_rx) = channel();
     let main_tx_switch1 = Sender::clone(&main_tx);
     let main_tx_switch2 = Sender::clone(&main_tx);
 
-
     // Initialise buttons.
     let mut switch1 = Gpio::new().unwrap().get(17).unwrap().into_input();
-    switch1.set_async_interrupt(Trigger::FallingEdge, move |level| {
-        main_tx_switch1.send(MainMessage::ButtonChange(1, level)).unwrap();
-        }).unwrap();
- 
+    switch1
+        .set_async_interrupt(Trigger::FallingEdge, move |level| {
+            main_tx_switch1
+                .send(MainMessage::ButtonChange(1, level))
+                .unwrap();
+        })
+        .unwrap();
     let mut switch2 = Gpio::new().unwrap().get(26).unwrap().into_input();
-    switch2.set_async_interrupt(Trigger::FallingEdge, move |level| {
-        main_tx_switch2.send(MainMessage::ButtonChange(2, level)).unwrap();
-        }).unwrap();
-     
+    switch2
+        .set_async_interrupt(Trigger::FallingEdge, move |level| {
+            main_tx_switch2
+                .send(MainMessage::ButtonChange(2, level))
+                .unwrap();
+        })
+        .unwrap();
 
-
-    spawn(move || {
-        loop {
-            let dt = Local::now();
-            let time_to_sleep = Duration::new(0, 1_000_000_000 - dt.nanosecond());
-            sleep(time_to_sleep);
-            main_tx.send(MainMessage::TimeSignal).unwrap();
-        }
+    // Create "pendulum" process and set it to generate a message every second,
+    // on the second.
+    spawn(move || loop {
+        let dt = Local::now();
+        let time_to_sleep = Duration::new(0, 1_000_000_000 - dt.nanosecond());
+        sleep(time_to_sleep);
+        main_tx.send(MainMessage::TimeSignal).unwrap();
     });
 
+    // Create the web server process.
+    spawn(move || webserver());
+
+    // Main message loop.
     for main_message in main_rx {
         match main_message {
             MainMessage::TimeSignal => {
@@ -80,10 +91,9 @@ fn main() {
                     disp_time(&display_tx, display_inverted);
                 } else {
                     display_intensity = (display_intensity + 1) % 16;
-                    display_tx.send((0xA, display_intensity)).unwrap();  // Set intensity.
+                    display_tx.send((0xA, display_intensity)).unwrap(); // Set intensity.
                 }
-
-            } 
+            }
         }
     }
 }
@@ -92,108 +102,161 @@ fn disp_time(display_tx: &std::sync::mpsc::Sender<(u8, u8)>, display_inverted: b
     let dt = Local::now();
     let hour_high: u8 = (dt.hour() / 10) as u8;
     let hour_low: u8 = (dt.hour() % 10) as u8;
-    let minute_high: u8 = (dt.minute() /10) as u8;
+    let minute_high: u8 = (dt.minute() / 10) as u8;
     let minute_low: u8 = (dt.minute() % 10) as u8;
     let second_high: u8 = (dt.second() / 10) as u8;
-    let second_low: u8 = (dt.second() % 10) as u8;        
+    let second_low: u8 = (dt.second() % 10) as u8;
     if display_inverted {
-        display_tx.send((0x8, decode_digit(second_low, DigitOrientation::Inverted, false))).unwrap();
-        display_tx.send((0x7, decode_digit(second_high, DigitOrientation::Inverted, false))).unwrap();
+        display_tx
+            .send((
+                0x8,
+                decode_digit(second_low, DigitOrientation::Inverted, false),
+            ))
+            .unwrap();
+        display_tx
+            .send((
+                0x7,
+                decode_digit(second_high, DigitOrientation::Inverted, false),
+            ))
+            .unwrap();
         display_tx.send((0x6, 0x1)).unwrap();
-        display_tx.send((0x5, decode_digit(minute_low, DigitOrientation::Inverted, false))).unwrap();
-        display_tx.send((0x4, decode_digit(minute_high, DigitOrientation::Inverted, false))).unwrap();
+        display_tx
+            .send((
+                0x5,
+                decode_digit(minute_low, DigitOrientation::Inverted, false),
+            ))
+            .unwrap();
+        display_tx
+            .send((
+                0x4,
+                decode_digit(minute_high, DigitOrientation::Inverted, false),
+            ))
+            .unwrap();
         display_tx.send((0x3, 0x1)).unwrap();
-        display_tx.send((0x2, decode_digit(hour_low, DigitOrientation::Inverted, false))).unwrap();
-        display_tx.send((0x1, decode_digit(hour_high, DigitOrientation::Inverted, false))).unwrap();
+        display_tx
+            .send((
+                0x2,
+                decode_digit(hour_low, DigitOrientation::Inverted, false),
+            ))
+            .unwrap();
+        display_tx
+            .send((
+                0x1,
+                decode_digit(hour_high, DigitOrientation::Inverted, false),
+            ))
+            .unwrap();
     } else {
-        display_tx.send((0x1, decode_digit(second_low, DigitOrientation::Normal, false))).unwrap();
-        display_tx.send((0x2, decode_digit(second_high, DigitOrientation::Normal, false))).unwrap();
+        display_tx
+            .send((
+                0x1,
+                decode_digit(second_low, DigitOrientation::Normal, false),
+            ))
+            .unwrap();
+        display_tx
+            .send((
+                0x2,
+                decode_digit(second_high, DigitOrientation::Normal, false),
+            ))
+            .unwrap();
         display_tx.send((0x3, 0x1)).unwrap();
-        display_tx.send((0x4, decode_digit(minute_low, DigitOrientation::Normal, false))).unwrap();
-        display_tx.send((0x5, decode_digit(minute_high, DigitOrientation::Normal, false))).unwrap();
+        display_tx
+            .send((
+                0x4,
+                decode_digit(minute_low, DigitOrientation::Normal, false),
+            ))
+            .unwrap();
+        display_tx
+            .send((
+                0x5,
+                decode_digit(minute_high, DigitOrientation::Normal, false),
+            ))
+            .unwrap();
         display_tx.send((0x6, 0x1)).unwrap();
-        display_tx.send((0x7, decode_digit(hour_low, DigitOrientation::Normal, false))).unwrap();
-        display_tx.send((0x8, decode_digit(hour_high, DigitOrientation::Normal, false))).unwrap();
+        display_tx
+            .send((0x7, decode_digit(hour_low, DigitOrientation::Normal, false)))
+            .unwrap();
+        display_tx
+            .send((
+                0x8,
+                decode_digit(hour_high, DigitOrientation::Normal, false),
+            ))
+            .unwrap();
     }
 }
 
 fn decode_digit(digit: u8, orientation: DigitOrientation, dp: bool) -> u8 {
     let mut return_value = 0;
     match orientation {
-        DigitOrientation::Normal => {
-            match digit {
-                0 => {
-                    return_value += 126;
-                }
-                1 => {
-                    return_value += 48;
-                }
-                2 => {
-                    return_value += 109;
-                }
-                3 => {
-                    return_value += 121;
-                }
-                4 => {
-                    return_value += 51;
-                }
-                5 => {
-                    return_value += 91;
-                }
-                6 => {
-                    return_value += 95;
-                }
-                7 => {
-                    return_value += 112;
-                }
-                8 => {
-                    return_value += 127;
-                }
-                9 => {
-                    return_value += 123;
-                }
-                _ => {
-                    return_value += 0;
-                }
+        DigitOrientation::Normal => match digit {
+            0 => {
+                return_value += 126;
             }
-        }
-        DigitOrientation::Inverted => {
-            match digit {
-                0 => {
-                    return_value += 126;
-                }
-                1 => {
-                    return_value += 6;
-                }
-                2 => {
-                    return_value += 109;
-                }
-                3 => {
-                    return_value += 79;
-                }
-                4 => {
-                    return_value += 23;
-                }
-                5 => {
-                    return_value += 91;
-                }
-                6 => {
-                    return_value += 123;
-                }
-                7 => {
-                    return_value += 14;
-                }
-                8 => {
-                    return_value += 127;
-                }
-                9 => {
-                    return_value += 95;
-                }
-                _ => {
-                    return_value += 0;
-                }
+            1 => {
+                return_value += 48;
             }
-        }
+            2 => {
+                return_value += 109;
+            }
+            3 => {
+                return_value += 121;
+            }
+            4 => {
+                return_value += 51;
+            }
+            5 => {
+                return_value += 91;
+            }
+            6 => {
+                return_value += 95;
+            }
+            7 => {
+                return_value += 112;
+            }
+            8 => {
+                return_value += 127;
+            }
+            9 => {
+                return_value += 123;
+            }
+            _ => {
+                return_value += 0;
+            }
+        },
+        DigitOrientation::Inverted => match digit {
+            0 => {
+                return_value += 126;
+            }
+            1 => {
+                return_value += 6;
+            }
+            2 => {
+                return_value += 109;
+            }
+            3 => {
+                return_value += 79;
+            }
+            4 => {
+                return_value += 23;
+            }
+            5 => {
+                return_value += 91;
+            }
+            6 => {
+                return_value += 123;
+            }
+            7 => {
+                return_value += 14;
+            }
+            8 => {
+                return_value += 127;
+            }
+            9 => {
+                return_value += 95;
+            }
+            _ => {
+                return_value += 0;
+            }
+        },
     }
     if dp {
         return_value += 1;
@@ -201,11 +264,43 @@ fn decode_digit(digit: u8, orientation: DigitOrientation, dp: bool) -> u8 {
     return_value
 }
 
+fn webserver() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "src/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "src/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 enum DigitOrientation {
     Normal,
     Inverted,
 }
-
 
 enum MainMessage {
     TimeSignal,
